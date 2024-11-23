5c6f4612c7339b8ee7c9277c3a1bc6dce8a402d6
Parvez-pabel <pabelalomparvez2233@gmail.com>
2024-11-24 02:14:47 +0600

    initial components function

---
+++ src/components/invoice/ReviewSubtimButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewSubtimButton = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ReviewSubtimButton;
---
+++ src/components/layout/AppNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppNav = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AppNav;
